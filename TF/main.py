c384adc1758f9bbdb6abedabbc0ee734726aa170
adam jacobs <adamjacobs272@gmail.com>
2024-10-25 11:27:55 -0400

    testing random inputs

--- TF/main.py
+++ TF/main.py
@@ -1,11 +1,14 @@
 from ai_edge_litert.interpreter import Interpreter
-
+import numpy as np
+import time
 # feature contains input_ids, input_mask and segment_ids, which are all
 # retrieved from input preprocessing, and are tensors of shape
 # [batch_size, max_seq_length], where max_seq_length = 384.
 
 # Load TFLite model and allocate tensors.
-tflite_file = '1.tflite'
+
+
+tflite_file = './1.tflite'
 interpreter = Interpreter(model_path=tflite_file)
 interpreter.allocate_tensors()
 
@@ -13,23 +16,17 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Test model on random input data.
-input_ids = feature["input_ids"]
-input_mask = feature["input_mask"]
-segment_ids = feature["segment_ids"]
-
-input_ids = np.array(input_ids, dtype=np.int32)
-input_mask = np.array(input_mask, dtype=np.int32)
-segment_ids = np.array(segment_ids, dtype=np.int32)
-
-interpreter.set_tensor(input_details[0]["index"], input_ids)
-interpreter.set_tensor(input_details[1]["index"], input_mask)
-interpreter.set_tensor(input_details[2]["index"], segment_ids)
+# Test the model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.int32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+start_time = time.time()
 interpreter.invoke()
+end_time = time.time()
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+#print(output data)
+print("elapsed time", end_time - start_time, "seconds")
 
-# Get output logits.
-end_logits = interpreter.get_tensor(output_details[0]["index"])[0]
-start_logits = interpreter.get_tensor(output_details[1]["index"])[0]
 
-print(start_logits)
-print(end_logits)
