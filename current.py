5872157ecd1af705ca4dede36ee700e5d981546b
josephckarch <karchjoseph@gmail.com>
2025-02-18 10:34:32 -0500

    updated current.py

--- current.py
+++ current.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #Data for Multimeter
 MM_Data = {
-    'ip_addr' : ('10.245.26.218', 5555),
+    'ip_addr' : ('10.245.26.169', 5555),
     'curr_dc' : ':MEAS:CURR:DC?\n'
 }
 
@@ -20,22 +20,29 @@ MM_SOCKET.settimeout(2.0)
 RECV_MAX_BYTES = 1024
 
 Times = []
-for x in range(300):
-    Times.append(0.1 + (x * 0.1))
-
 Measurements = []
+
 time.sleep(0.1)
-for x in range(300):
+totalTime = 0
+for x in range(60):
+    # start timer
+    start = time.perf_counter()
     #Query the MM
     MM_SOCKET.send(bytes(MM_Data['curr_dc'], "utf-8"))
     #Store that value
     current = float((MM_SOCKET.recv(RECV_MAX_BYTES)).decode("utf-8"))
-    Measurements.append(current)
-    time.sleep(0.1)
+    # stop timer
+    end = time.perf_counter()
+    # keeo track of total time
+    totalTime += end - start
+    # y axis
+    Measurements.append(current * 3.3)
+    # x axis
+    Times.append(totalTime)
 
 #Plot data and labels
 plt.plot(Times, Measurements)
 plt.xlabel("Time(s)")
-plt.ylabel("Current(A)")
-plt.title("Measured Current Over 30 Seconds")
+plt.ylabel("Power(W)")
+plt.title("Measured Power Over 25 Seconds")
 plt.show()
